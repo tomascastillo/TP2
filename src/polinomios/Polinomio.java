9ac61dc8d7992ff58d7b5656083c14568fe16b3f
Adrian Radice <radice.adrian.sjt@gmail.com>
2017-09-10 11:00:18 -0300

    HORNER

--- src/polinomios/Polinomio.java
+++ src/polinomios/Polinomio.java
@@ -5,13 +5,25 @@ public class Polinomio {
 	private int grado;
 	private double[] coeficientes;
 	
-	public Polinomio(){}
+	public Polinomio(int grado, double[] coeficientes){
+		this.grado = grado;
+		this.coeficientes = coeficientes;
+	}
 	
 	public double evaluarMSucesivas (double x){ return 0; }
 	public double evaluarRecursiva (double x){ return 0; }
 	public double evaluarRecursivaPar (double x) { return 0; }
 	public double evaluarProgDinamica (double x) { return 0; }
 	public double evaluarMejorada (double x){return 0;}
-	public double evaluarHorner ( double x ) { return 0; }
+	
+	/**
+	 * HORNER=> O(n)
+	 */
+	public double evaluarHorner ( double x ) { 
+		double res = 0;
+		for (int i=0; i <= grado; res = res*x + coeficientes [i], i++ );
+		return res;	
+	}
+	
 	
 }
