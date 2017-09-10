b61c5f205f03ee97c86b429897ddd10f9213aecb
Adrian Radice <radice.adrian.sjt@gmail.com>
2017-09-10 10:27:12 -0300

    Preparando entorno

---
+++ src/polinomios/Polinomio.java
@@ -0,0 +1,17 @@
+package polinomios;
+
+public class Polinomio {
+
+	private int grado;
+	private double[] coeficientes;
+	
+	public Polinomio(){}
+	
+	public double evaluarMSucesivas (double x){ return 0; }
+	public double evaluarRecursiva (double x){ return 0; }
+	public double evaluarRecursivaPar (double x) { return 0; }
+	public double evaluarProgDinamica (double x) { return 0; }
+	public double evaluarMejorada (double x){return 0;}
+	public double evaluarHorner ( double x ) { return 0; }
+	
+}
